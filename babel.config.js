160a0afb546c974faa3a0c08a1c2e8f936a1ddd3
Felipe.DS <Felipe.DS@outlook.com>
2020-10-22 18:59:30 -0300

    chore: babel added

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+  ]
+}
