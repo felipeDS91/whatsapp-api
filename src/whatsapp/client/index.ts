c814d804d2203fe2f50952b5a377be13af5c173a
felipdDS91 <felipe.ds@outlook.com>
2022-05-06 23:26:56 -0300

    chore: config puppeteer

--- src/whatsapp/client/index.ts
+++ src/whatsapp/client/index.ts
@@ -37,7 +37,20 @@ class Whatsapp {
   private async initializeClient(clientId: string = "") {
     this.client = new Client({
       authStrategy: new LocalAuth({ dataPath: 'tokens', clientId: clientId }),
-      puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] },
+      puppeteer: {
+        headless: true,
+        args: [
+          '--no-sandbox',
+          '--unhandled-rejections=strict',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process', // <- this one doesn't works in Windows
+          '--disable-gpu'
+        ],
+      },
     });
 
     this.client.on('qr', async qr => {
