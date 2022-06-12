5110c6c2ee87a72092b41d0d34b9d0185de673c9
felipdDS91 <felipe.ds@outlook.com>
2022-06-12 14:32:22 -0300

    chore: pm2 config file changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,8 @@ module.exports = {
   apps: [{
     name: "whatsapp-api",
     script: "./dist/server.js",
-    watch: true,
   }, {
     name: 'whatsapp-worker',
     script: './dist/whatsapp/index.js',
-    watch: true,
   }]
 }
