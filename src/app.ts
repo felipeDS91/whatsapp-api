1b395ee3e9d62004221d75f0bc136b93ed7a42d0
felipdDS91 <felipe.ds@outlook.com>
2023-02-05 22:21:43 -0300

    feat: body maximum size increased

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import AppError from './errors/AppError';
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '15mb' }));
 app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
