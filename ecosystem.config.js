587a261b6f4f115fa83abbe301a4b3f223f1d02c
felipdDS91 <felipe.ds@outlook.com>
2023-05-15 21:20:09 -0300

    feat: restart delay

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [{
     name: "whatsapp-api",
     script: "./dist/server.js",
-    restart_delay: 5000,
+    restart_delay: 30000,
   }, {
     name: 'whatsapp-worker',
     script: './dist/whatsapp/index.js',
-    restart_delay: 5000,
+    restart_delay: 30000,
   }]
 }
